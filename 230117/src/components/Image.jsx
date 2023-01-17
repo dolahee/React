fdb3ddfb9da3b9e47b3901dbb885e1436f33da21
dolahee <155641@naver.com>
2023-01-17 15:58:56 +0900

    images

--- 230117/src/components/Image.jsx
+++ 230117/src/components/Image.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import lostArkImg from "../img.png";
 
 export default function Image() {
-  return <img src="/images/img.png" alt="로아이미지" />;
+  return (
+    <>
+      <img src="/images/img.png" alt="로아이미지" />
+      <img src={lostArkImg} alt="로아이미지" />;
+    </>
+  );
 }
