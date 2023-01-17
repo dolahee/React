ef6430b3c20fc9b1afe18bd5170e4b327dd58bee
dolahee <155641@naver.com>
2023-01-17 13:59:58 +0900

    230117

---
+++ 230117/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UseEffectFetch from "./components/UseEffectFetch";
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectFetch />
+    </div>
+  );
+}
+
+export default App;
